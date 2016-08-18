516d50f35eb514fd77d08c3e51bf50547e5a9515
WangCongwei <wangcw@huoli.com>
2016-08-18 10:40:03 +0800

    ci config update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,11 @@
-echo 'done'
+set -e
+
+echo 'start deploy'
+ssh root@128.199.69.214 << EOF
+  cd /root/code/github/notepad.cc
+  git pull
+  npm install
+  pm2 restart pm2.json
+EOF
+echo 'done deploy'
+
