ae1e3c2437fa6cdf7ad5ead7905da7c66c6d9068
WangCongwei <wangcw@huoli.com>
2016-08-26 11:53:23 +0800

    use sw-precache to handle service worker stuff

--- public/script/main.js
+++ public/script/main.js
@@ -115,13 +115,27 @@ function startSyncNote() {
 
 
 function startServiceWorker() {
-  if (location.protocol === 'https:' && 'serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./serviceWorker.js')
-      .then(function(reg) {
-        console.info('[sw] register')
-      })
-      .catch(function(err) {
-        console.error('[sw] Failed to register service worker. ', err)
-      })
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js').then(function(reg) {
+      reg.onupdatefound = function() {
+        var installingWorker = reg.installing
+        installingWorker.onstatechange = function() {
+          switch (installingWorker.state) {
+            case 'installed':
+              if (navigator.serviceWorker.controller) {
+                console.log('New or updated content is available.')
+              } else {
+                console.log('Content is now available offline!')
+              }
+              break
+            case 'redundant':
+              console.error('The installing service worker became redundant.')
+              break
+          }
+        }
+      }
+    }).catch(function(e) {
+      console.error('Error during service worker registration:', e)
+    })
   }
 }
