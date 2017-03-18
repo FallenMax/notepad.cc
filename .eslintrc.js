9bd79fc2d3a8baaa16ed811fa8a70916ff0d81c8
FallenMax <fallenmax@gmail.com>
2017-03-18 21:05:16 +0800

    add eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    'no-console': ['warn'],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never']
+  }
+};
