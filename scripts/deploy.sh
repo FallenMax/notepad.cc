2ffc3f18b82f5eaefebcd34527dcbe865067787e
FallenMax <FallenMax@gmail.com>
2018-07-02 00:51:32 +0800

    backup before deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,10 +2,13 @@ set -e
 
 echo 'start deploy'
 ssh root@128.199.240.2 -p9999 << EOF
+  set -e
+  rm -rf /root/code/github/notepad.cc_bak
+  cp -r /root/code/github/notepad.cc{,_bak}
   cd /root/code/github/notepad.cc
   git pull
-  npm install
-  npm run build
+  yarn install
+  yarn run build
   pm2 startOrRestart pm2.json
 EOF
 echo 'done deploy'
