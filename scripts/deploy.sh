e86171d65db85233642c7d9899f5056cc0923955
FallenMax <wangcongwei@shimo.im>
2019-04-05 23:12:17 +0800

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,31 +1,59 @@
+
 set -e
 
-echo 'start deploy'
+target=$1
+user=root
+host=139.59.222.188
+port=9999
+download_dir=/root/download
 
-scp -P 9999 bundle.zip root@139.59.222.188:/root/code/github/notepad.cc
+if [ $target = 'dev' ]
+then
+  app_dir=/root/apps/notepad_dev
+  data_dir=/var/lib/notepad_dev/data
+  start_cmd="pm2 startOrRestart pm2_dev.json"
+elif [ $target = 'prod' ]
+then
+  app_dir=/root/apps/notepad
+  data_dir=/var/lib/notepad/data
+  start_cmd="pm2 startOrRestart pm2_prod.json"
+else
+  echo 'unknown target'
+  exit -1
+fi
 
-ssh root@139.59.222.188 -p9999 << EOF
+echo "deploying: $target"
+
+
+npm run build
+npm run test
+npm run bundle
+
+scp -P $port bundle.zip $user@$host:$download_dir
+
+
+ssh $user@$host -p$port << EOF
   set -e
 
   echo 'deploy started...'
-  cd /root/code/github/notepad.cc
+  cd $download_dir
 
-  echo 'backup stuff...'
-  rm -rf backup
-  mkdir -p backup
-  mv -f public server package.json yarn.lock pm2.json backup/
-
-  echo 'unpack bundle...'
-  unzip bundle.zip
-  rm -rf bundle.zip
+  echo 'replacing bundle...'
+  rm -rf $app_dir
+  unzip bundle.zip -d $app_dir
+  rm bundle.zip
+  cd $app_dir
 
   echo 'install dependencies...'
   yarn install
 
-  echo 'restart app...'
-  pm2 startOrRestart pm2.json
+  echo 'starting app...'
+  $start_cmd
+  pm2 ls
 
   echo 'deploy done'
 EOF
+
 echo 'done deploy'
+
 
