6bf68520847d38a00d905bb784ce482e6265b923
FallenMax <wangcongwei@shimo.im>
2019-10-11 22:38:59 +0800

    use proper start/end symbols...

--- src/client/lib/assist/assistor.types.ts
+++ src/client/lib/assist/assistor.types.ts
@@ -16,10 +16,10 @@ export interface Assistor {
 }
 
 // TODO
-export const START = '左'
-export const END = '右'
-// export const START = '\u0000'
-// export const END = '\u0001'
+// export const START = '左'
+// export const END = '右'
+export const START = '\u0000'
+export const END = '\u0001'
 export const CURSOR = `${START}${END}`
 export const BULLET = '-'
 export const NOT_BULLET = '[^-]'
